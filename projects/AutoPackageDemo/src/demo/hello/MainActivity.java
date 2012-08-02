5129aaab332d3dc80c2334c1b322d3e16630c6ac
13leaf <sd6733531@gmail.com>
2012-08-02 17:10:52 +0800

    增加示例项目和配置文件

---
+++ projects/AutoPackageDemo/src/demo/hello/MainActivity.java
@@ -0,0 +1,32 @@
+package demo.hello;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+String PUB_ID="www";
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        PackageManager manager=getPackageManager();
+        try {
+			PackageInfo info=manager.getPackageInfo("demo.hello", PackageManager.GET_ACTIVITIES);
+			//show version
+			Toast.makeText(this, "version:"+info.versionName+"\nversionCode:"+info.versionCode, Toast.LENGTH_LONG).show();
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+        
+		TextView textView=(TextView) findViewById(R.id.text);
+		textView.setText(PUB_ID);
+    }
+}
